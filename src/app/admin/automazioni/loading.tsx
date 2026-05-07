bafc74deb441c48cbcb011658aed3004b5ba19f2
samueleberetta <ulbereten@gmail.com>
2026-05-07 11:04:10 +0200

    remove: delete automazioni loading

--- src/app/admin/automazioni/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="w-8 h-8 border-4 border-retro-border border-t-neon-blue rounded-full animate-spin" />
-    </div>
-  );
-}
